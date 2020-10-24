7fcc81980ff44ea7ad1ba4efdab008b6b4f4be6b
Simon Johansson <simonljohansson92@gmail.com>
2020-10-24 21:55:19 +0200

    feat(factory): Add updateCallExpression factory function

--- packages/factory/src/index.ts
+++ packages/factory/src/index.ts
@@ -18,4 +18,14 @@ export const createPropertyAssignment = (
   name: string | ts.Identifier | ts.PrivateIdentifier | ts.StringLiteral | ts.NumericLiteral | ts.ComputedPropertyName
 ) => (initializer: ts.Expression) => ts.factory.createPropertyAssignment(name, initializer)
 
-export const createIdentifier = ts.factory.createIdentifier
+export const updateCallExpression = (
+  identifier?: string | ts.Identifier,
+  typeArguments?: readonly ts.TypeNode[],
+  argumentsArray?: readonly ts.Expression[]
+) => (callExpression: ts.CallExpression) =>
+  ts.factory.updateCallExpression(
+    callExpression,
+    typeof identifier === "string" ? createIdentifier(identifier) : identifier,
+    typeArguments || [],
+    argumentsArray || []
+  );
