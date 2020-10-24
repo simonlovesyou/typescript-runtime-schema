7f52457f5e1f7744281aa686ca5fe68d0401eb63
Simon Johansson <simonljohansson92@gmail.com>
2020-10-24 21:55:40 +0200

    chore(transform): Lint

--- packages/factory/src/index.ts
+++ packages/factory/src/index.ts
@@ -8,15 +8,25 @@ export const createCallExpression = (
 
 export const createPropertyAccessExpression = (
   name: string | ts.Identifier | ts.PrivateIdentifier
-) => (expression: ts.Expression) => ts.factory.createPropertyAccessExpression(expression, name)
+) => (expression: ts.Expression) =>
+  ts.factory.createPropertyAccessExpression(expression, name);
 
-export const createObjectLiteralExpression = (
-  multiLine?: boolean
-) => (properties?: readonly ts.ObjectLiteralElementLike[]) => ts.factory.createObjectLiteralExpression(properties, multiLine)
+export const createObjectLiteralExpression = (multiLine?: boolean) => (
+  properties?: readonly ts.ObjectLiteralElementLike[]
+) => ts.factory.createObjectLiteralExpression(properties, multiLine);
 
 export const createPropertyAssignment = (
-  name: string | ts.Identifier | ts.PrivateIdentifier | ts.StringLiteral | ts.NumericLiteral | ts.ComputedPropertyName
-) => (initializer: ts.Expression) => ts.factory.createPropertyAssignment(name, initializer)
+  name:
+    | string
+    | ts.Identifier
+    | ts.PrivateIdentifier
+    | ts.StringLiteral
+    | ts.NumericLiteral
+    | ts.ComputedPropertyName
+) => (initializer: ts.Expression) =>
+  ts.factory.createPropertyAssignment(name, initializer);
+
+export const createIdentifier = ts.factory.createIdentifier;
 
 export const updateCallExpression = (
   identifier?: string | ts.Identifier,
