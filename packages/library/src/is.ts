981603aca61148cbfa43c3010e6da49b36e9df6c
Simon Johansson <simonljohansson92@gmail.com>
2020-10-25 11:45:51 +0100

    feat(library): Add validation logic

--- packages/library/src/is.ts
+++ packages/library/src/is.ts
@@ -1,3 +1,32 @@
-const is = <T>(value: any): value is T => value
+import Joi from "joi";
 
-export default is
+interface SchemaDescriptor {
+  type: "number" | "string" | "object" | "any" | "boolean";
+  required?: boolean;
+  optional?: boolean;
+}
+
+const convertSchemaDescriptorToSchema = (
+  schemaDescriptor: SchemaDescriptor
+): Joi.Schema => Joi[schemaDescriptor.type]();
+
+const is = <T>(
+  value: any,
+  schemaDescriptor: SchemaDescriptor | SchemaDescriptor[]
+): value is T => {
+  if (Array.isArray(schemaDescriptor)) {
+    const schemaDescriptors = schemaDescriptor;
+
+    return (
+      Joi.alternatives()
+        .try(...schemaDescriptors.map(convertSchemaDescriptorToSchema))
+        .validate(value).error === null
+    );
+  }
+  return (
+    convertSchemaDescriptorToSchema(schemaDescriptor).validate(value).error ===
+    undefined
+  );
+};
+
+export default is;
