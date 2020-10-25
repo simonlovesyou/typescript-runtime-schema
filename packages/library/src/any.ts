f70aa5c4993efcd6a5e03b32ffe8140280cd7f76
Simon Johansson <simonljohansson92@gmail.com>
2020-10-25 11:43:24 +0100

    chore(library): Remove not used utility types

--- packages/library/src/any.ts
+++
@@ -1,2 +0,0 @@
-export type Allow<T extends any, V extends any> = Valid<V, T>
-export type Valid<T extends any, V extends any> = T
--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,10 +1,3 @@
-import * as any from './any'
-import * as string from './string'
 import is from './is'
-
-export {
-  any,
-  string
-}
 
 export default is
