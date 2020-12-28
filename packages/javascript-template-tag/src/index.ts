3b682436df68bc8074a58dbb91187732fc9535d3
Simon Johansson <simonljohansson92@gmail.com>
2020-12-28 17:07:23 +0100

    docs(javascript-template-tag): Add documentation

--- packages/javascript-template-tag/src/index.ts
+++ packages/javascript-template-tag/src/index.ts
@@ -1,10 +1,11 @@
-import { parseScript }from 'escaya'
+import { parseScript } from "escaya";
+
 /**
  * Attempt to parse the code in the provided template tag (Only accepts one for now)
  * @param strings - TemplateStringsArray
  * @public
  */
-export default (strings: TemplateStringsArray) => {
-  parseScript(strings[0])
-  return strings[0]
-}
+export default function javascriptTemplatetag(strings: TemplateStringsArray) {
+  parseScript(strings[0]);
+  return strings[0];
+};
