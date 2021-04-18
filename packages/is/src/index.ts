344937b4bf116a819076e58c4ceaf2dbc13ca24e
John Wright <jrite@protonmail.com>
2021-04-18 14:11:04 +0100

    fix: jsonschema uses draft7

--- packages/is/src/index.ts
+++ packages/is/src/index.ts
@@ -1,9 +1,8 @@
-import JSONSchema4 from "json-schema";
-import { Validator as SchemaValidator } from "jsonschema";
+import { Schema, Validator as SchemaValidator } from "jsonschema";
 /**
  * @public
  */
-const is = <T>(schema: JSONSchema4.JSONSchema4) => (value: any): value is T => {
+const is = <T>(schema: Schema) => (value: any): value is T => {
   const validator = new SchemaValidator();
 
   return validator.validate(value, schema).valid;
